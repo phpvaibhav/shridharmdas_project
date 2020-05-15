07ef5d3d614d2b5df89d394f0a09ca6da292a831
unknown <php.vaibhav@gmail.com>
2020-05-15 17:39:23 +0530

    trash export fgf

--- etoh.php
+++ etoh.php
@@ -1,4 +1,41 @@
 <html>
+<head>
+<title>How to Convert Hindi Text into English Using JavaScript?</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+<link href="http://www.google.com/uds/modules/elements/transliteration/api.css"
+type="text/css" rel="stylesheet"/>
+</head>
+<body>
+<h1>How to Convert Hindi Text into English Using JavaScript?</h1><br>
+<h2>Type in Hindi (Press Ctrl+g to toggle between English and Hindi)</h2>
+<div id="Wrapper"> <h2> Textarea </h2>
+<textarea id="transliterateTextarea" style="width:650px;height:300px"></textarea>
+</div>
+</body>
+<script type="text/javascript">
+// Load the Google Transliteration API
+google.load("elements", "1", {
+packages: "transliteration"
+});
+function onLoad() {
+var options = {
+sourceLanguage:
+google.elements.transliteration.LanguageCode.ENGLISH,
+destinationLanguage:
+google.elements.transliteration.LanguageCode.HINDI,
+shortcutKey: 'ctrl+g',
+transliterationEnabled: true
+};
+var control =
+new google.elements.transliteration.TransliterationControl(options);
+control.makeTransliteratable(['transliterateTextarea']);
+}
+google.setOnLoadCallback(onLoad);
+</script>
+</html>
+
+<!-- <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -30,4 +67,4 @@
     <textarea id="english2hindi" style="width:600px;height:200px"></textarea>
  Press Ctrl+g to toggle between English and Hindi
   </body>
-</html>
+</html> -->
