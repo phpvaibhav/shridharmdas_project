af76a591a18fb6b30369db8576831fe2fd8f337a
unknown <php.vaibhav@gmail.com>
2020-06-03 11:57:36 +0530

    sub admin correction

--- application/modules/admin/controllers/Adminrole.php
+++ application/modules/admin/controllers/Adminrole.php
@@ -17,7 +17,7 @@ class Adminrole extends Common_Back_Controller {
         $count          = $this->common_model->get_total_count('admin',array('roleId !='=>1));
         $data['countuser']  = $count ;   
         $count              = number_format_short($count);
-        $data['recordSet']  = array('<li class="sparks-info"><h5>'.lang('Sub_Admin').'<span class="txt-color-blue"><a  href="'.base_url('sub-admin-add').'" class="anchor-btn" ><i class="fa fa-plus-square"></i></a></span></h5></li>','<li class="sparks-info"><h5>'.lang('Total').' '.lang('Users').'<span class="txt-color-darken" id="totalCust"><i class="fa fa-lg fa-fw fa fa-users"></i>&nbsp;'.$count.'</span></h5></li>');
+        $data['recordSet']  = array('<li class="sparks-info"><h5>'.lang('Sub_Admin').'<span class="txt-color-blue"><a  href="'.base_url('sub-admin-add').'" class="anchor-btn" ><i class="fa fa-plus-square"></i></a></span></h5></li>','<li class="sparks-info"><h5>'.lang('Total').' '.lang('Sub_Admin').'<span class="txt-color-darken" id="totalCust"><i class="fa fa-lg fa-fw fa fa-users"></i>&nbsp;'.$count.'</span></h5></li>');
      
         $data['front_scripts']  = array('backend_assets/custom/js/common_datatable.js');
         $this->load->admin_render('adminrole/index', $data, '');
